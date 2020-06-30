ce51a6b6d2bfb9d9641ed4cf08a2ebb93dfbcec7
imogenkraak <imogen.kraak@gmail.com>
2020-06-30 19:36:52 +0100

    Pods commit

---
+++ Pods/Target Support Files/Pods-eating in admin/Pods-eating in admin-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_eating_in_admin : NSObject
+@end
+@implementation PodsDummy_Pods_eating_in_admin
+@end
---
+++ Pods/Target Support Files/Pods-eating in/Pods-eating in-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_eating_in : NSObject
+@end
+@implementation PodsDummy_Pods_eating_in
+@end
